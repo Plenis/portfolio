c5efb7031546f2d846b109030d6818169f662fe5
Sino Plenis <plenissino@gmail.com>
2021-01-16 15:40:08 +0200

    first draft of personal profile

---
+++ src/main/java/net/webapp/App.java
@@ -0,0 +1,63 @@
+package net.webapp;
+
+import org.jdbi.v3.core.Jdbi;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+//import java.sql.SQLException;
+import java.util.*;
+//import java.util.List;
+
+import static spark.Spark.*;
+
+public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
+    static Jdbi getJdbiDatabaseConnection(String defaultJdbcUrl) throws URISyntaxException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String database_url = processBuilder.environment().get("DATABASE_URL");
+        if (database_url != null) {
+
+            URI uri = new URI(database_url);
+            String[] hostParts = uri.getUserInfo().split(":");
+            String username = hostParts[0];
+            String password = hostParts[1];
+            String host = uri.getHost();
+
+            int port = uri.getPort();
+
+            String path = uri.getPath();
+            String url = String.format("jdbc:postgresql://%s:%s%s", host, port, path);
+
+            return Jdbi.create(url, username, password);
+        }
+
+        return Jdbi.create(defaultJdbcUrl);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            port(getHerokuAssignedPort());
+            staticFiles.location("/public");
+
+            Jdbi jdbi = getJdbiDatabaseConnection("jdbc:postgresql://localhost/waiters_app?username=sino&password=123");
+
+            Map<String, Object> map = new HashMap<>();
+
+
+            get("/", (request, response) -> new ModelAndView(map, "profile.handlebars"), new HandlebarsTemplateEngine());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
